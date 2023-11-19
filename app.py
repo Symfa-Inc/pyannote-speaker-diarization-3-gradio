dc37b9801f065d28cc2c6c2bd492bdb6983cfee4
Ilya Mokin <ilia.mokin@gmail.com>
2023-11-19 14:03:33 +0100

    updated launch

--- app.py
+++ app.py
@@ -1,3 +1,35 @@
 import gradio as gr
+import torch
+import os
+from pyannote.audio import Pipeline
 
-gr.load("models/pyannote/speaker-diarization-3.1").launch()
+
+read_key = os.environ.get('HF_TOKEN', None)
+
+pipeline = Pipeline.from_pretrained(
+  "pyannote/speaker-diarization-3.1",
+  use_auth_token=read_key)
+
+pipeline.to(torch.device("cuda"))
+
+def transcribe_speech(filepath):
+    output = pipeline(filepath)
+    return output
+
+app = gr.Blocks()
+
+file_transcribe = gr.Interface(
+    fn=transcribe_speech,
+    inputs=gr.Audio(source="upload", type="filepath"),
+    outputs=gr.outputs.Textbox(),
+)
+
+
+with app:
+    gr.TabbedInterface(
+        [file_transcribe],
+        ["Transcribe Audio File"],
+    )
+
+app.launch()
+gr.load("models/pyannote/speaker-diarization-3.1", hf_token=read_key).launch()
